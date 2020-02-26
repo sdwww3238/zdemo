d059bdb032aa9adc90c7d7835dcda9f903ce98c4
mike <jian6663238@163.com>
2020-02-26 21:18:30 +0800

    first

---
+++ auser/views.py
@@ -0,0 +1,6 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse('hello,这是第一次用djongo写项目')
